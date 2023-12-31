636136a5fbc2a0954a2a71c1e9a842a6d60f8313
exceed <exceed@mayi888.com>
2023-12-31 17:46:29 +0800

    Fix 修复文字驱动声音TTS功能

--- src/utils/text2speech.py
+++ src/utils/text2speech.py
@@ -5,15 +5,16 @@ from TTS.api import TTS
 
 class TTSTalker():
     def __init__(self) -> None:
-        self.tts = TTS(TTS.list_models()[0])
+        model_name = TTS.list_models()[0]
+        self.tts = TTS(model_name)
 
-    def test(self, text, audio, language='en'):
+    def test(self, text, language='en'):
 
         tempf  = tempfile.NamedTemporaryFile(
                 delete = False,
                 suffix = ('.'+'wav'),
             )
 
-        self.tts.tts_to_file(text, speaker_wav=audio, language=language, file_path=tempf.name)
+        self.tts.tts_to_file(text, speaker=self.tts.speakers[0], language=language, file_path=tempf.name)
 
         return tempf.name
