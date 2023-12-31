de25764d1314c3d497d04df39b5f4335e0d26642
exceed <exceed@mayi888.com>
2023-12-31 21:02:14 +0800

    使用GPU加快text2speech

--- src/utils/text2speech.py
+++ src/utils/text2speech.py
@@ -6,7 +6,7 @@ from TTS.api import TTS
 class TTSTalker():
     def __init__(self) -> None:
         model_name = "tts_models/multilingual/multi-dataset/xtts_v2"
-        self.tts = TTS(model_name=model_name)
+        self.tts = TTS(model_name=model_name, progress_bar=True, gpu=True)
 
     def test(self, text, audio, language='en'):
 
